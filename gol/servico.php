5a56e263c9b271c41e23fa15fa8d97f661cb46d3
Jonasqwerty <106714952+Jonasqwerty@users.noreply.github.com>
2023-07-20 13:27:05 -0300

    servico.php

--- gol/servico.php
+++ gol/servico.php
@@ -2,8 +2,20 @@
 
     $data = file_get_contents('php://input');
     $obj = json_decode( $data, true );
-    $sql = "select origem, destino, datahora from voo";
-    $conexao = new pdo('sqlite:banco.sqlite');
-    $resultado = $conexao->query($sql)->fetchAll();
-    $data = json_encode( $resultado );
-    print $data;
+    $conexao = new pdo('sqlite:database');
+    $origem = $obj["origem"];
+    $destino = $obj["destino"];
+    $datahora = $obj["datahora"];
+    
+    if ($origem){
+        $select = " select * from vvoo where origem='$origem' and destino='$destino' and datahora='$datahora' ; ";
+        $resultado = $conexao->query($select)->fetchAll();
+        $result = json_encode($resultado);
+        error_log($result);
+        print $result;
+        return;
+    }
+    $select = " select * from vvoo; ";
+    $resultado = $conexao->query($select)->fetchAll();
+    $result = json_encode( $resultado );
+    print $result;
